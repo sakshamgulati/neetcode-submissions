6e0dc5195ebb1fe012af17c9cc8208931542128b
SakshamGulati <sakshamgulati123@gmail.com>
2026-07-29 19:53:17 -0400

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        sumRob=[0]*len(nums)
+        i=0
+        maxReturn=0
+        while i < len(nums):
+            sumRob[i] += nums[i] if i < 2 else max(sumRob[i-2],sumRob[i-3])+nums[i]
+            maxReturn = max(sumRob[i],maxReturn)
+            i+=1
+        return maxReturn
+        
